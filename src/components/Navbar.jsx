fc4a9d12ad6749316160418345d20dfd76fd66d6
Dalton Ehret <daltonehret@gmail.com>
2021-01-07 16:18:14 -0600

    added Navbar functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 8px;
+`;
+
+const StyledNavLink = styled(NavLink)`
+    color: white;
+    padding: 12px;
+    
+    :hover {
+        background-color: red;
+    }
+`;
+
+export const Navbar = () => {
+    return (
+        <StyledDiv>
+            <StyledNavLink to={'/'}>{'Home'}</StyledNavLink>
+            <StyledNavLink to={'/about'}>{'About'}</StyledNavLink>
+            <StyledNavLink to={'/projects'}>{'Projects'}</StyledNavLink>
+            <StyledNavLink to={'/contact'}>{'Contact'}</StyledNavLink>
+        </StyledDiv>
+    )
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Projects = () => {
+    return (
+        <div>
+            <h1>{'These are My Projects'}</h1>
+        </div>
+    );
+};
