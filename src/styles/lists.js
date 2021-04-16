6a71ec18fa8139a355dcebc13b66a8c2df20181b
daltonehret@gmail.com <daltonehret@gmail.com>
2021-04-16 12:02:39 -0500

    Added a styled list

---
+++ src/styles/lists.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledList = styled.li`
+    width: 25%;
+    height: auto;
+    margin-top: 10px;
+    margin-right: 10px;
+    display: inline-block;
+    // border: 1px solid red;
+`;
