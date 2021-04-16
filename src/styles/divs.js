269ba4f4171fe3411e0a143d4e2fd3d38c84dd6c
daltonehret@gmail.com <daltonehret@gmail.com>
2021-04-16 12:02:30 -0500

    Create new div element

--- src/styles/divs.js
+++ src/styles/divs.js
@@ -3,10 +3,22 @@ import styled from "styled-components";
 export const StyledDiv = styled.div`
     background-color: #262525;
     bottom: 0;
+    height: max-content;
     margin-left: 25%;
+    min-height: 100%;
     position: absolute;
     top: 0;
     width: 75%;
+`;
+
+export const DottedBorderedDiv = styled.div`
+    background-color: #5c5b5b;
+    border-radius: 15px;
+    border: 2px dotted white;
+    color: white;
+    margin: 24px;
+    padding: 8px;
+    width: 50%;
 `;
 
 export const BorderedDiv = styled.div`
