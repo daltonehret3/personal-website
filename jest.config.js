5297d23e0fda98459792c451979944ea74717f94
Dalton Ehret <daltonehret@gmail.com>
2022-02-20 01:07:12 -0600

    Fix :shirt:

--- jest.config.js
+++ jest.config.js
@@ -26,5 +26,6 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules/'],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js", "\\.(css|less)$": "<rootDir>/assetsTransformer.js"
-    }
+    },
+    testEnvironment: 'jsdom'
 };
